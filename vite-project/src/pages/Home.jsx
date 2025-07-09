31216ee7fbad7109460b501a565354d96063913b
Vedant Parikh <pariv3722@wrdsb.ca>
2025-07-09 00:09:37 -0400

    final setup

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+    return <div> 
+        
+        {/* Theme Toggle */}  
+
+        {/* Effects for Background */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+        
+        </div>;
+};
