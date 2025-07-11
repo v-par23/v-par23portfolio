9a66b117afb76f6db1a5cc2970f27be5f044ab20
Vedant Parikh <pariv3722@wrdsb.ca>
2025-07-10 23:39:07 -0400

    toggle setup

---
+++ vite-project/src/components/ThemeToggle.jsx
@@ -0,0 +1,9 @@
+import { Moon, Sun } from "lucide-react"; 
+import { useState } from "react"; 
+
+export const ThemeToggle = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    
+    return <button> {isDarkMode ? <Sun /> : <Moon />}</button>;
+
+};
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import {ThemeToggle} from "../components/ThemeToggle.jsx";
 export const Home = () => {
-    return <div className = "min-h-screen "> 
+    return (
+    <div className = "min-h-screen bg-background text-foreground overflow-x-hidden"> 
         
         {/* Theme Toggle */}  
+        <ThemeToggle />
 
         {/* Effects for Background */}
 
@@ -11,5 +14,6 @@ export const Home = () => {
 
         {/* Footer */}
         
-        </div>;
+        </div>
+    );
 };
