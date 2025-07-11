e1c1c62919a35a44923a802514822658b8f1f0d3
Vedant Parikh <109480819+v-par23@users.noreply.github.com>
2025-07-11 15:50:11 -0400

    Update Home.jsx

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import {ThemeToggle} from "../components/ThemeToggle.jsx";
+import {StarBackground} from "../components/StarBackground.jsx";
+
 export const Home = () => {
     return (
     <div className = "min-h-screen bg-background text-foreground overflow-x-hidden"> 
@@ -7,6 +9,7 @@ export const Home = () => {
         <ThemeToggle />
 
         {/* Effects for Background */}
+        <StarBackground />
 
         {/* Navbar */}
 
