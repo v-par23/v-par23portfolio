e08e072033607e0a6293db6b2c8b77144eb4885b
Vedant Parikh <109480819+v-par23@users.noreply.github.com>
2025-07-13 01:02:23 -0400

    fixed positioning for theme toggle button

--- vite-project/src/components/ThemeToggle.jsx
+++ vite-project/src/components/ThemeToggle.jsx
@@ -31,7 +31,7 @@ export const ThemeToggle = () => {
 
     return (
         <button onClick={toggleTheme} className = {cn(
-            "fixed top-5 right-5 z-50 p-2 rounded-full transition-colours duration-300",
+            "fixed top-5 right-0 z-50 p-2 rounded-full transition-colors duration-300",
             "focus: outline-hidden"
         )}
         > 
