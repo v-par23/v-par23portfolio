354bd63126739d4546949d933a92cf109a3ee698
Vedant Parikh <109480819+v-par23@users.noreply.github.com>
2025-07-13 00:42:37 -0400

    finish HeroSection.jsx

--- vite-project/src/components/HeroSection.jsx
+++ vite-project/src/components/HeroSection.jsx
@@ -5,7 +5,29 @@ export const HeroSection = () => {
             className = "relative min-h-screen flex flex-col items-center justify-center px-4"
         >
                 <div className = "container max-w-4xl mx-auto text-center z-10">
-                    
+                    <div className = "space-y-6">
+                        <h1 className = "text-4xl md:text-6xl font-bold tracking-light">
+                            <span className = "opacity-0 animate-fade-in"> Hi, I'm</span>
+                            <span className = "text-primary opacity-0 animate-fade-in-delay-1"> 
+                                {" "}
+                                Vedant
+                            </span>
+                            <span className = "text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> 
+                                {" "}
+                                Parikh
+                            </span>
+                        </h1>
+                        <p className = "text-xl md:text-xl text-muted-foreground/80 max-w-full lg:whitespace-nowrap opacity-0 animate-fade-in-delay-3" >
+                            {/*software engineer, open-source contributor, and sport enthusiast | Systems Design Engineering at UWaterloo.*/} 
+                            problem solver fueled by code, open-source, and sport | Systems Design Engineering at UWaterloo.
+                        </p>
+
+                        <div className = "pt-4 opacity-0 animate-fade-in-delay-4">
+                            <a href = "#projects" className = "cosmic-button">
+                                View My Work
+                            </a>
+                        </div>
+                    </div>    
                 </div>
         </section>
     )
